2fd7c66d20f3e5ee9f7366efaa316b532d6484e3
elianismedina <elianismedina05@outlook.com>
2025-02-10 14:07:16 -0500

    added sidebar to admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,32 +1,13 @@
-import getSession from "../../lib/getSession";
-import { Metadata } from "next";
+import React from "react";
 
-import { redirect } from "next/navigation";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Admin",
 };
 
-export default async function Page() {
-  const session = await getSession();
-  const user = session?.user;
+const AdminPage = () => {
+  return <div>Admin Page Content</div>;
+};
 
-  if (!user) {
-    redirect("/api/auth/signin?callbackUrl=/admin");
-  }
-
-  if (user.role !== "ADMIN") {
-    return (
-      <main className="mx-auto my-10">
-        <p className="text-center">You are not authorized to view this page</p>
-      </main>
-    );
-  }
-
-  return (
-    <main className="mx-auto my-10 space-y-3">
-      <h1 className="text-center text-xl font-bold">Admin Page</h1>
-      <p className="text-center">Welcome, admin!</p>
-    </main>
-  );
-}
+export default AdminPage;
